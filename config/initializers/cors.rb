e44fea5df2bf4de638ee4910d0e640e320fcf0b4
Jellis <venetianself@hotmail.com>
2017-09-05 13:09:11 -0500

    changed origin to *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://noticeboard-general-assembly.herokuapp.com/notices'
+    origins '*'
 
     resource '*',
       headers: :any,
